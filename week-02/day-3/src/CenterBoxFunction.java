6f7b9edb53c8db52a26e4a229d52a51c5759ff37
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-08 22:27:56 +0200

    Ranbow Box

--- week-02/day-3/src/CenterBoxFunction.java
+++ week-02/day-3/src/CenterBoxFunction.java
@@ -13,18 +13,30 @@ public class CenterBoxFunction {
     // draw 3 squares with that function.
     // avoid code duplication.
 
-    int width;
-    int height;
+//    int width;
+//    int height;
+//    for (int i = 1; i <= 3; i += 1) {
+//      width = (int) (Math.random() * 350);
+//      height = (int) (Math.random() * 350);
+//      centerBox(width, height, graphics);
+//    }
+
+//    int size = (int) (Math.random() * 250);  //if it is outside of the for loop maybe it is not working or it does not draw 3 squares
     for (int i = 1; i <= 3; i += 1) {
-      width = (int) (Math.random() * 350);
-      height = (int) (Math.random() * 350);
-      centerBox(width, height, graphics);
+      int size = (int) (Math.random() * 250);
+      centerBox(size, graphics);
     }
   }
-  public static void centerBox(int width, int height, Graphics graphics) {
+  public static void centerBox(int size, Graphics graphics) {
     graphics.setColor(Color.red);
-    graphics.drawRect(WIDTH / 2 - width / 2, HEIGHT / 2 - height / 2, width, height);
+    graphics.drawRect(WIDTH / 2 - size / 2, HEIGHT / 2 - size / 2, size, size);
   }
+
+//  public static void centerBox(int width, int height, Graphics graphics) {
+//    graphics.setColor(Color.red);
+//    graphics.drawRect(WIDTH / 2 - width / 2, HEIGHT / 2 - height / 2, width, height);
+//  }
+
 
 //  public static void centerBoxFunction(int x, Graphics graphics); {
 

a92e8e52801ad59ba616617fd305bca1ffa47940
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-11 21:02:54 +0200

    Drawing a Square

---
+++ week-01/day-5/src/DrawSquare.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DrawSquare {
+  public static void main(String[] args) {
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%
+    // %   %
+    // %   %
+    // %   %
+    // %   %
+    // %%%%%
+    //
+    // The square should have as many lines as the number was
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me a number, please!");
+    int number = scanner.nextInt();
+
+    for (int row = 1; row <= number; row += 1) {
+      for (int column = 1; column <= number; column += 1) {
+        if (row == 1 || row == number || column == 1 || column == number) {
+          System.out.print("+");
+        }
+        else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}

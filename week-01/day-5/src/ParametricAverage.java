4f76fea9eafb0146ba1f1cd20f12bcfe19ac98dd
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-12 12:11:30 +0200

    Parametric average kinda final

--- week-01/day-5/src/ParametricAverage.java
+++ week-01/day-5/src/ParametricAverage.java
@@ -9,7 +9,9 @@ public class ParametricAverage {
     //
     // Sum: 22, Average: 4.4
 
-    int lengthOfTheNumbers;                                                 //initializing an unknown length of numbers
+    int lengthOfTheNumbers;
+    double sum = 0;
+    double average = 0;
     Scanner scanner = new Scanner(System.in);
     System.out.println("How many numbers do you want to declare?");
     lengthOfTheNumbers = scanner.nextInt();
@@ -17,11 +19,16 @@ public class ParametricAverage {
     int[] numbers = new int[lengthOfTheNumbers];
 
     for (int counter = 0; counter < lengthOfTheNumbers; counter += 1) {
-      System.out.print("Your " + (counter + 1) + " number is: ");
+      System.out.print("Your " + (counter + 1) + ". number is: ");
       numbers[counter] = scanner.nextInt();
     }
     scanner.close();
 
-    
+   for (int i = 0; i < lengthOfTheNumbers; i += 1) {
+     sum += numbers[i];
+     average = sum / lengthOfTheNumbers;
+   }
+    System.out.println("The sum of your numbers is: " + sum);
+    System.out.println("The average of your numbers is: " + average);
   }
 }

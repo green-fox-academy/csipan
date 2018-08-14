1e55b19d88d79dffe1f258c3a7f64e51726b7fe1
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-14 12:03:03 +0200

    DivideByZero

--- week-03/day-2/src/DivideByZero.java
+++ week-03/day-2/src/DivideByZero.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DivideByZero {
   public static void main(String[] args) {
     // Create a function that takes a number
@@ -5,6 +7,18 @@ public class DivideByZero {
     // and prints the result.
     // It should print "fail" if the parameter is 0
 
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me a number and I will dived 10 with it");
+    int number = scanner.nextInt();
+    recallFunction(number);
   }
-}
+
+    public static void recallFunction(int number){
+      try {
+        int result = 10 / number;
+        System.out.println(result);
+      } catch (ArithmeticException e) {
+        System.out.println("fail");
+      }
+    }
+  }

35691cc8eb9e9e98a6d7b581af13bfc3b255c6aa
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-14 13:49:05 +0200

    PrintEachLine finished

--- week-03/day-2/src/PrintEachLine.java
+++ week-03/day-2/src/PrintEachLine.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class PrintEachLine {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     // Write a program that opens a file called "my-file.txt", then prints
     // each of the lines form the file.
     // If the program is unable to read the file (for example it does not exists),
@@ -13,11 +13,12 @@ public class PrintEachLine {
 
 
     try {
-      Path filePath = Paths.get("c:/Users/Csipan/greenfox/csipan/week-03/day-2/my-file.txt");
+      Path filePath = Paths.get("C:/Users/Csipan/greenfox/csipan/week-03/day-2/my-file.txt");
       List<String> lines = Files.readAllLines(filePath);
       System.out.println(lines);
     } catch (IOException exeption) {
       System.out.println("Uh-oh, could not read the file!");
+      exeption.printStackTrace();
     }
   }
 }

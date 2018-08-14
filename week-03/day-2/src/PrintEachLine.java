38de6d3ea755f032998a8f53fa7c9b508d3d0dd2
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-14 13:07:39 +0200

    PrintEachLine strugeling

--- week-03/day-2/src/PrintEachLine.java
+++ week-03/day-2/src/PrintEachLine.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.Files;
 import java.util.List;
 
 public class PrintEachLine {
@@ -12,12 +12,12 @@ public class PrintEachLine {
     // then it should print an error message like: "Unable to read file: my-file.txt"
 
 
-//    try {
-      Path filePath = Paths.get("c:\\Users\\Csipan\\greenfox\\csipan\\week-03\\day-2\\my-file.txt");
+    try {
+      Path filePath = Paths.get("c:/Users/Csipan/greenfox/csipan/week-03/day-2/my-file.txt");
       List<String> lines = Files.readAllLines(filePath);
-      System.out.println(lines.get(0));
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
+      System.out.println(lines);
+    } catch (IOException exeption) {
+      System.out.println("Uh-oh, could not read the file!");
+    }
   }
 }

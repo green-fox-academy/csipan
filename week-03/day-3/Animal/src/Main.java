653837b59574cabd474a92cf208dc02706b10734
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-15 20:32:33 +0200

    Animal

---
+++ week-03/day-3/Animal/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    Animal firstAnimal = new Animal();
+    firstAnimal.name = "Buksi a kutya";
+    System.out.println(firstAnimal.name);
+
+    for (int i = 0; i < 50; i += 1) {
+      firstAnimal.eat();
+      firstAnimal.drink();
+    }
+    firstAnimal.play();
+    System.out.println("Hunger and thirst level is: " + firstAnimal.hunger + ", " + firstAnimal.thirst);
+  }
+}

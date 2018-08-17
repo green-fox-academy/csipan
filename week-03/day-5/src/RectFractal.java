46af5d5082b010e52d897475392a926e4fb1a532
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-17 11:45:10 +0200

    RectFractal final

--- week-03/day-5/src/RectFractal.java
+++ week-03/day-5/src/RectFractal.java
@@ -9,41 +9,36 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RectFractal {
   public static void mainDraw(Graphics graphics){
-    int size = 600;
-    int startingPositionX = 0;
-    int startingPositionY = 0;
-    graphics.setColor(Color.yellow);
-    graphics.fillRect(startingPositionX, startingPositionY, size * 3, size * 3);
+    int size = 1000;
+    int startingPositionX = 10;
+    int startingPositionY = 10;
 
     drawFractal(startingPositionX, startingPositionY, size, graphics);
-    drawRectangle(startingPositionX, startingPositionY, size, graphics);
-
   }
 
   public static void drawRectangle(int x, int y, int size, Graphics graphics) {
-
+    graphics.setColor(Color.yellow);
+    graphics.fillRect(x, y, size, size);
     graphics.setColor(Color.black);
-    graphics.drawRect(x, y, size * 3, size *3);
-    graphics.drawRect(x + size, y, size, size);
-    graphics.drawRect(x, y + size, size, size);
-    graphics.drawRect(x + size, y + 2 * size, size, size);
-    graphics.drawRect(x + 2 * size, y + size, size, size);
+    graphics.drawRect(x, y, size, size);
+
   }
 
   public static void drawFractal(int x, int y, int size, Graphics graphics) {
     drawRectangle(x, y, size, graphics);
-    if (size > 20) {
-      drawFractal(x + size, y + size / 3, size / 9, graphics);
-      drawFractal(x + size * 5 / 3, y + size / 3, size / 9, graphics);
-      drawFractal(x + size * 4 / 3, y, size / 9, graphics);
-      drawFractal(x + size * 4 / 3, y + size * 2 / 3, size / 9, graphics);
+    if (size > 10) {
+      drawFractal(x, y + size / 3, size / 3, graphics);
+      drawFractal(x + size * 2 / 3, y + size / 3, size / 3, graphics);
+      drawFractal(x + size * 1 / 3, y, size / 3, graphics);
+      drawFractal(x + size * 1 / 3, y + size * 2 / 3, size / 3, graphics);
+
     }
   }
 
 
   // Don't touch the code below
-  static int WIDTH = 600;
-  static int HEIGHT = 600;
+  static int WIDTH = 620;
+  static int HEIGHT = 643;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");

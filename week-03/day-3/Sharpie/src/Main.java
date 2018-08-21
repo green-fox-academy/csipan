90bd34430ce9005f132cbcd446fe8a540bbd7911
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-21 15:59:29 +0200

    Sharpie

--- week-03/day-3/Sharpie/src/Main.java
+++ week-03/day-3/Sharpie/src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Main {
+  public static void main(String[] args) {
+
+
+    Sharpie sharpie = new Sharpie("black", 25, 75);
+    Sharpie sharpie1 = new Sharpie("yellow", 22, 69);
+
+//    sharpie.color = "black";
+//    sharpie.width = 25;
+    if (sharpie.width > 20 ) {
+      sharpie.use();
+      sharpie1.use();
+    }
+    System.out.println("The sharpie color is: " + sharpie.color + ", " + sharpie.width + " width and the ink amount is: " + sharpie.inkAmount);
+    System.out.println("The sharpie color is: " + sharpie1.color + ", " + sharpie1.width + " width and the ink amount is: " + sharpie1.inkAmount);
+  }
 }
--- week-03/day-3/Sharpie/src/Sharpie.java
+++ week-03/day-3/Sharpie/src/Sharpie.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Sharpie {
+
+  String color;
+  double width;
+  double inkAmount;
+
+  public Sharpie(String color, double width, double inkAmount) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = inkAmount;
+  }
+
+  public void use() {
+    inkAmount -= 1.0;
+  }
 }

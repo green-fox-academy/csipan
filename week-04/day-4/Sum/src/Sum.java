fd2ed630cd0b2cfeea4e6b707cab9d5ba6879963
Chippy <csipan_mihalyalbert@yahoo.com>
2018-08-23 15:29:11 +0200

    Sum

--- week-04/day-4/Sum/src/Sum.java
+++ week-04/day-4/Sum/src/Sum.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
 
 public class Sum {
+  ArrayList<Integer> listOfNumbers = new ArrayList<>();
 
-  public int sum(ArrayList<Integer> list) {
+  public void addNumber(int numbers, int numbers1) {
+    listOfNumbers.add(numbers);
+    listOfNumbers.add(numbers1);
+  }
+
+  public int sumOfTheList() {
     int sum = 0;
-    list = new ArrayList<>();
-    for (int i = 0; i < list.size(); i += 1) {
-      sum += 1;
+        for (int i = 0; i < listOfNumbers.size(); i += 1) {
+      sum += listOfNumbers.get(i);
     }
     return sum;
   }
---
+++ week-04/day-4/Sum/test/SumTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+  @Test
+  public void addNumberToListGetSum () {
+    Sum newNumber = new Sum();
+    newNumber.addNumber(15, 16);
+    Assert.assertEquals(31, newNumber.sumOfTheList());
+  }
+
+}

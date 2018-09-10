99a7757a4b1ce84937dfb6237b601c854a89f987
csipan <csipan_mihalyalbert@yahoo.com>
2018-09-10 15:45:49 +0200

    exercise 3 done

--- week-06/day-1/src/Exercuse2.java
+++ week-06/day-1/src/Exercuse2.java
@@ -8,7 +8,7 @@ public class Exercuse2 {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
     System.out.println(numbers.stream()
             .filter(n -> n % 2 != 0)
-            .mapToInt(n -> n)
+            .mapToDouble(n -> n)
             .average());
   }
 }

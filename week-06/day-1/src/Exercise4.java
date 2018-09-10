802603257fd949f1775a7036c3bacdeed111bdb9
csipan <csipan_mihalyalbert@yahoo.com>
2018-09-10 15:51:55 +0200

    exercise 4 done

---
+++ week-06/day-1/src/Exercise4.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to find which number squared value is more then 20 from the following array:
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+    List<Integer> numbersSíuerBiggerThanTwenty = numbers.stream()
+            .filter(n -> n * n > 20)
+            .collect(Collectors.toList());
+    System.out.println(numbersSíuerBiggerThanTwenty);
+  }
+}

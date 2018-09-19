e8e02426f31962f058276f8e8361a1b97b640f7d
csipan <csipan_mihalyalbert@yahoo.com>
2018-09-19 15:16:21 +0200

    Useful Utilities exercise done

---
+++ week-07/day-3/useful-utilities/src/main/java/com/greenfoxacademy/usefulutilities/controller/UtilityController.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.usefulutilities.controller;
+
+import com.greenfoxacademy.usefulutilities.service.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UtilityController {
+
+  UtilityService utilityService;
+
+  @Autowired
+  public UtilityController(UtilityService utilityService) {
+    this.utilityService = utilityService;
+  }
+
+  @GetMapping(value = "/useful")
+  public String showColoredBackground() {
+    return "useful";
+  }
+
+  @ GetMapping(value = "/useful/colored")
+  public String showRandomColorBackground(Model model) {
+    model.addAttribute("backgroundColor", utilityService.randomColor());
+    return "colored";
+  }
+}
